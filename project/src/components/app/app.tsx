2e69164dae91c98ef8f563da9ddfcc1bd4d400ec
Никита <kondor12371@yandex.ru>
2022-02-11 17:44:23 +0300

    Создаст компоненты экранов приложения

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPageScreen from '../main-page-screen/main-page-screen';
+
+type AppScreenProps = {
+  movieTitle: string;
+  movieGenre: string;
+  startDate: string;
+}
+
+function App({movieTitle, movieGenre, startDate}: AppScreenProps): JSX.Element {
+  return (
+    <MainPageScreen
+      movieTitle={movieTitle}
+      movieGenre={movieGenre}
+      startDate={startDate}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const MovieInfo = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  startDate: '2014',
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      movieTitle={MovieInfo.title}
+      movieGenre={MovieInfo.genre}
+      startDate={MovieInfo.startDate}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
