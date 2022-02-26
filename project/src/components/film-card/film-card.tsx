f2ea269fda0dd7f598e3ff9de41f26f6b621d6e0
Никита <kondor12371@yandex.ru>
2022-02-26 16:07:08 +0300

    Добавит компонент Video
    Сделана возможность просмотра превью фильма при наведении на карточку фильма

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,23 +1,43 @@
 import { Link } from 'react-router-dom';
 import { Film } from '../../types/films';
+import Player from '../player/player';
 
 type FilmCardProps = {
   film: Film;
   onHover: (cardId: number) => void;
+  isActive: boolean;
 }
 
 function FilmCard(props: FilmCardProps) {
-  const {film, onHover} = props;
-  const {name, posterImage, id} = film;
+  const {film, onHover, isActive} = props;
+  const {name, id, posterImage, videoLink} = film;
+
+  let playTimer: NodeJS.Timeout;
+
   return (
     <article
-      onMouseOver={
-        () => {onHover(id);}
+      onMouseEnter={
+        () => {
+          playTimer = setTimeout(() => {
+            onHover(id);
+          }, 1000);
+        }
+      }
+      onMouseLeave={
+        () => {
+          clearTimeout(playTimer);
+          onHover(0);
+        }
       }
       className="small-film-card catalog__films-card"
     >
       <div className="small-film-card__image">
-        <img src={posterImage} alt={name} width="280" height="175" />
+        <Player
+          src={videoLink}
+          posterImage={posterImage}
+          isActive={isActive}
+          isPreview
+        />
       </div>
       <h3 className="small-film-card__title">
         <Link
--- project/src/components/film-cards/film-cards.tsx
+++ project/src/components/film-cards/film-cards.tsx
@@ -7,22 +7,25 @@ type FilmCardsProps = {
 }
 
 function FilmCards({films}: FilmCardsProps) {
-  const [, setActiveCard] = useState(0);
+  const [activeCard, setActiveCard] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
   function cardHoverHandler(cardId: number) {
     setActiveCard(cardId);
+    setIsActive(cardId !== 0);
   }
 
   return (
     <div className="catalog__films-list">
       {
         films.map((film) => {
-          const keyValue = `${film.id}`;
+          const keyValue = `film-${film.id}`;
           return (
             <FilmCard
               key={keyValue}
               film={film}
               onHover={cardHoverHandler}
+              isActive={isActive && film.id === activeCard}
             />
           );
         })
